35683368e68a01388c6925d7230fa09c48221b1b
thomas hunckler <th@phytodia.com>
2023-10-16 16:41:19 +0200

    change

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "pages#home"
+  root to: "pages#home"
   get 'espace_pro/index'
 end
