ca740e2f8b8633769c83698fefb18aefbace4399
thomas hunckler <th@phytodia.com>
2023-10-19 09:39:54 +0200

    install gem simple form and country

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,10 @@
 class ProfilesController < ApplicationController
   def index
+    @profiles = Profile.all
   end
 
   def show
+    @profile = Profile.find(params[:id])
   end
 
   def new
@@ -10,6 +12,9 @@ class ProfilesController < ApplicationController
   end
 
   def create
+    @profile = Profile.new(params[:profile])
+    @profile.save
+    redirect_to profile_path(@profile)
   end
 
   def edit
@@ -19,5 +24,10 @@ class ProfilesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def profile_param
+    params.require(:profile).permit(:firstname,:lastname,:role,:address,:city,:cp,:country,:tel)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  get 'profiles/index'
-  get 'profiles/show'
-  get 'profiles/new'
-  get 'profiles/create'
-  get 'profiles/edit'
-  get 'profiles/update'
-  get 'profiles/destroy'
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root to: "pages#home"
   get 'espace_pro/index'
+  resources :profiles
+
 end
