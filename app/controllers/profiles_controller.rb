108cbf843be9d5c48d1cc2116cd86627db1d97b0
thomas hunckler <th@phytodia.com>
2023-10-19 10:40:10 +0200

    change

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,5 @@
 class ProfilesController < ApplicationController
+
   def index
     @profiles = Profile.all
   end
@@ -8,11 +9,17 @@ class ProfilesController < ApplicationController
   end
 
   def new
+    #@user = User.new
     @profile = Profile.new
+    #@profile = Profile.new
+    #@profile.build_user
   end
 
   def create
+    new_user = User.new(emai:params[:profile][:user][:email],password:params[:profile][:user][:password])
+    fail
     @profile = Profile.new(params[:profile])
+    fail
     @profile.save
     redirect_to profile_path(@profile)
   end
@@ -28,6 +35,6 @@ class ProfilesController < ApplicationController
 
   private
   def profile_param
-    params.require(:profile).permit(:firstname,:lastname,:role,:address,:city,:cp,:country,:tel)
+    params.require(:profile).permit(:firstname,:lastname,:role,:address,:city,:cp,:country,:tel,users_attributes: [:email,:password])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  has_one :profile, dependent: :destroy
+  accepts_nested_attributes_for :profile
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
