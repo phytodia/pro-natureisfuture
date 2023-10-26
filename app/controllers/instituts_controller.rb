e5fe347d3c82b8a22e74e7586f333b67e87d26f6
thomas hunckler <th@phytodia.com>
2023-10-26 18:29:28 +0200

    change

--- app/controllers/instituts_controller.rb
+++ app/controllers/instituts_controller.rb
@@ -6,7 +6,8 @@ class InstitutsController < ApplicationController
     @markers = @flats.geocoded.map do |flat|
       {
         lat: flat.latitude,
-        lng: flat.longitude
+        lng: flat.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: {flat: flat})
       }
   end
   end
