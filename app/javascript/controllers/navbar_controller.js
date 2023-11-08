32a539cca93312ebdbb60907a5e998393047b23f
thomas hunckler <th@phytodia.com>
2023-11-08 14:52:17 +0100

    change img on hover

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navbar"
 export default class extends Controller {
-  static targets = ["submenu", "menugreen"]
+  static targets = ["submenu", "menugreen", "logo"]
   connect() {
     console.log("navbar stimulus")
 
@@ -17,5 +17,16 @@ export default class extends Controller {
   mouseOut(event) {
     event.currentTarget.querySelector(".dropdown-nav").style.visibility = "hidden";
     this.menugreenTarget.classList.remove("visible");
+  }
+  changeImg(event){
+    //alert(event.currentTarget);
+    //debugger;
+    event.currentTarget.src = "/assets/logo-nif-hover.png";
+    event.currentTarget.classList.add("change");
+  }
+  originalImg(event){
+    //alert(event.currentTarget);
+    event.currentTarget.src = "/assets/logo-nif-pro.png";
+    event.currentTarget.classList.remove("change");
   }
 }
