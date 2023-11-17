47492f5e1b812265fa52e318bf23f11c1daf608b
thomas hunckler <th@phytodia.com>
2023-11-17 14:56:34 +0100

    add menu mobile

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navbar"
 export default class extends Controller {
-  static targets = ["submenu", "menugreen", "logo"]
+  static targets = ["submenu", "menugreen", "logo","menuMobile"]
   connect() {
     console.log("navbar stimulus")
 
@@ -17,6 +17,12 @@ export default class extends Controller {
   mouseOut(event) {
     event.currentTarget.querySelector(".dropdown-nav").style.visibility = "hidden";
     this.menugreenTarget.classList.remove("visible");
+  }
+  toggleBurger(event){
+    //debbuger;
+    console.log("mobile click");
+    //debugger;
+    this.menuMobileTarget.classList.toggle("visible")
   }
 
 }
