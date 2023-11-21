230d21f65580debde9c76f5a1ddeb7a7edf9eb5f
thomas hunckler <th@phytodia.com>
2023-11-21 17:18:46 +0100

    change js navbar submenu how working

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navbar"
 export default class extends Controller {
-  static targets = ["submenu", "menugreen", "logo","menuMobile"]
+  static targets = ["submenu", "menugreen", "logo","menuMobile", "submenuMobile","menuprimaryMobile"]
   connect() {
     console.log("navbar stimulus")
 
@@ -22,10 +22,19 @@ export default class extends Controller {
     //debbuger;
     console.log("mobile click");
     //debugger;
-    this.menuMobileTarget.classList.toggle("visible")
+    this.menuMobileTarget.classList.toggle("visible");
+    this.submenuMobileTargets.forEach(element=>{element.classList.remove("visible")});
+    document.querySelectorAll(".submenu-items").forEach(element=>{element.classList.remove("visible")});
   }
   toggleSubmenu(event){
     event.currentTarget.querySelector("ul").classList.toggle("visible");
   }
+  voletSubmenu(event){
+    //debugger;
+    let indexMenu = Array.from(event.currentTarget.parentElement.children).indexOf(event.currentTarget);
+    this.menuprimaryMobileTarget.classList.remove("visible");
+    this.submenuMobileTargets[indexMenu].classList.add("visible");
+  }
+
 
 }
