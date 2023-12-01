40500b0549d992b4e5c4fe400b2f55f5a8cc0ff5
thomas hunckler <th@phytodia.com>
2023-12-01 15:26:13 +0100

    change

--- app/javascript/controllers/navbarmobile_controller.js
+++ app/javascript/controllers/navbarmobile_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navbarmobile"
 export default class extends Controller {
+  static targets = ["primarymenuMobile","voletMenu","submenuMobile"]
   connect() {
     console.log("new mobile navbar")
   }
@@ -12,5 +13,19 @@ export default class extends Controller {
   toggleMenu(){
     console.log("click sur menu mobile")
     document.querySelector(".mobile-full-main").classList.toggle("visible")
+  }
+  voletSubmenu(event){
+    //this.menuprimaryMobileTarget.classList.remove("visible")
+    event.currentTarget.parentElement.parentElement.parentElement.classList.remove("visible")
+
+    let volets = Array.from(document.querySelectorAll(".mobile-full-main"))
+    volets.forEach((element)=>{element.classList.remove("visible")})
+    volets[event.currentTarget.dataset.index].classList.add("visible")
+    //this.voletMenuTargets[event.currentTarget.dataset.index].classList.add("actif")
+  }
+  previousPanel(event){
+    let volets = Array.from(document.querySelectorAll(".mobile-full-main"))
+    volets.forEach((element)=>{element.classList.remove("visible")})
+    volets[event.currentTarget.dataset.index].classList.add("visible")
   }
 }
