24bcba4f4b75b33e8b3a8f7bb282ee7209e2e0f4
thomas hunckler <th@phytodia.com>
2023-12-08 12:00:56 +0100

    add reduce font when scroll

--- app/javascript/controllers/fixed_controller.js
+++ app/javascript/controllers/fixed_controller.js
@@ -9,11 +9,15 @@ export default class extends Controller {
     window.addEventListener("scroll", (event) => {
       if (window.scrollY >= 30) {
         //navtoFixed.classList.add("fixed");
-        document.getElementById("black-nav").classList.add("not-fixed")
+        document.getElementById("black-nav").classList.add("not-fixed");
+        document.getElementById("white-nav").classList.add("reduce-font");
+        document.getElementById("main-nav").classList.add("reduce-font");
           //nav.style.position = "fixed";
       } else {
         //navtoFixed.classList.remove("fixed");
         document.getElementById("black-nav").classList.remove("not-fixed")
+        document.getElementById("white-nav").classList.remove("reduce-font");
+        document.getElementById("main-nav").classList.remove("reduce-font");
       }
     });
   }
