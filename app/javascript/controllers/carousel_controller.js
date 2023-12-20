3448b299e579365610af5ceff6b3deb9dfe56b30
thomas hunckler <th@phytodia.com>
2023-12-20 15:56:11 +0100

    js working

--- app/javascript/controllers/carousel_controller.js
+++ app/javascript/controllers/carousel_controller.js
@@ -6,22 +6,39 @@ export default class extends Controller {
 
   initialize() {
     this.index = 0
+    //this.indexCarousel = 0
     this.showCurrentSlide()
   }
 
   next() {
-    this.index++
-    this.showCurrentSlide()
+    //sizeCarousel-=1
+    if (this.index < (this.slideTargets.length-=1)){
+      this.index++
+      console.log(`this index: ${this.index}`)
+      //this.indexCarousel++
+      console.log(`index Carousel : ${this.slideTargets.length}`)
+      this.showCurrentSlide()
+    }
+    else {
+    }
   }
 
   previous() {
-    this.index--
-    this.showCurrentSlide()
+    if (this.index <= (this.slideTargets.length) && this.index > 0){
+      this.index--
+      console.log(`this index: ${this.index}`)
+      //this.indexCarousel++
+      console.log(`index Carousel : ${this.slideTargets.length}`)
+      this.showCurrentSlide()
+    }
+    else {
+    }
   }
 
   showCurrentSlide() {
     this.slideTargets.forEach((element, index) => {
       element.hidden = index !== this.index
+      console.log(this.index)
     })
   }
 }
