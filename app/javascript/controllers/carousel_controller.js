35bcd0ce19ce2c47e091c0d192090091de921e56
thomas hunckler <th@phytodia.com>
2023-12-21 11:36:46 +0100

    change js carousel

--- app/javascript/controllers/carousel_controller.js
+++ app/javascript/controllers/carousel_controller.js
@@ -2,13 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="carousel"
 export default class extends Controller {
-  static targets = [ "slide" ]
-
+  static targets = [ "slide","slidemobile" ]
+  // concerne mobile + desktop
   initialize() {
     this.index = 0
     //this.indexCarousel = 0
     this.showCurrentSlide()
+    this.showmobileCurrentSlide()
   }
+  // fin initialize
 
   next() {
     //sizeCarousel-=1
@@ -41,4 +43,41 @@ export default class extends Controller {
       console.log(this.index)
     })
   }
+
+  nextmobile() {
+    //sizeCarousel-=1
+    if (this.index < (this.slidemobileTargets.length-=1)){
+      this.index++
+      console.log(`this index: ${this.index}`)
+      //this.indexCarousel++
+      console.log(`index Carousel : ${this.slidemobileTargets.length}`)
+      this.showmobileCurrentSlide()
+    }
+    else {
+    }
+  }
+
+  prevmobile() {
+    if (this.index <= (this.slidemobileTargets.length) && this.index > 0){
+
+      this.index--
+      console.log(`this index: ${this.index}`)
+      //this.indexCarousel++
+      console.log(`index Carousel : ${this.slidemobileTargets.length}`)
+      this.showmobileCurrentSlide()
+    }
+    else {
+
+    }
+  }
+
+  showmobileCurrentSlide() {
+
+    this.slidemobileTargets.forEach((element, index) => {
+      element.hidden = index !== this.index
+      console.log(this.index)
+    })
+  }
+
+
 }
