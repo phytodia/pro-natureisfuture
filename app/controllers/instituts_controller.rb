4e6142588c5b0203d25f45235bcff8e9f20e7152
thomas hunckler <th@phytodia.com>
2024-01-08 15:58:34 +0100

    add map

--- app/controllers/instituts_controller.rb
+++ app/controllers/instituts_controller.rb
@@ -15,6 +15,14 @@ class InstitutsController < ApplicationController
 
   def show
     @institut = Institut.find(params[:id])
+    @flat = @institut
+    @marker = [{
+        lat: @flat.latitude,
+        lng: @flat.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: {flat: @flat}),
+        marker_html: render_to_string(partial: "marker", locals: {flat: @flat})
+      }]
+  #[{:lat=>49.6312952, :lng=>1.8207573, :info_window_html=>"<h2>Test etablissement</h2>\n<p>13 rue principale</p>\n", :marker_html=>"<img height=\"30\" width=\"30\" alt=\"Logo\" src=\"/assets/logo-ecf839a7ae9d8c3650270ea01ceb8d45ef0f33459d9b14df8e780355ddf57533.png\" />\n"}]
   end
 
   def new
