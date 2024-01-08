2f9a6c233164c8622eaa37ff5a4fcb26aeedfb84
thomas hunckler <th@phytodia.com>
2024-01-08 12:56:42 +0100

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get '/contact', to: redirect('/instituts', status: 301) #Test redirection
+  #get '/contact', to: redirect('/instituts', status: 301) #Test redirection
   # Defines the root path route ("/")
   root to: "pages#home"
   get "contact", to: "pages#contact"
