7241a569ff600755a73dcfd7aebfda84761912b2
thomas hunckler <th@phytodia.com>
2024-01-09 12:56:47 +0100

    change

--- app/controllers/crm_controller.rb
+++ app/controllers/crm_controller.rb
@@ -70,10 +70,27 @@ class CrmController < ApplicationController
 
   def new_institut
     @institut = Institut.new
+    @institut.horaires = { monday: {am_1:"",am_2:"",pm_1:"",pm_2:""},tuesday: {am_1:"",am_2:"",pm_1:"",pm_2:""},wednesday: {am_1:"",am_2:"",pm_1:"",pm_2:""},thursday: {am_1:"",am_2:"",pm_1:"",pm_2:""},friday: {am_1:"",am_2:"",pm_1:"",pm_2:""},saturday: {am_1:"",am_2:"",pm_1:"",pm_2:""},sunday: {am_1:"",am_2:"",pm_1:"",pm_2:""}}
+    @days = [:monday, :tuesday,:wednesday,:thursday,:friday,:saturday,:sunday]
   end
 
   def create_institut
     @institut = Institut.new(institut_params)
+    x = institut_params[:horaires].to_hash.to_a.each_slice(4).to_a
+    days = [:monday,:tuesday,:wednesday,:thursday,:friday,:saturday,:sunday]
+    my_hash = {}
+    days.each do |day|
+      my_hash[day] = {am_1:"",am_2:"",pm_1:"",pm_2:""}
+    end
+    puts x.size
+    x.each do |sub_array|
+      sub_array.each_with_index do |element,index|
+          my_hash[days[index]][:am_1] = sub_array[0][1]
+          my_hash[days[index]][:am_2] = sub_array[1][1]
+          my_hash[days[index]][:pm_1] = sub_array[2][1]
+          my_hash[days[index]][:pm_2] = sub_array[3][1]
+      end
+    end
     fail
     if @institut.save
       redirect_to customer_crm_index_path(@institut.customer_id), notice: "L'établissement a été correctement crée"
@@ -96,6 +113,6 @@ class CrmController < ApplicationController
   end
 
   def institut_params
-    params.require(:institut).permit(:name,:tel,:address,:cp,:city,:country,:latitude,:longitude,:customer_id,:horaires)
+    params.require(:institut).permit(:name,:tel,:address,:cp,:city,:country,:latitude,:longitude,:customer_id,horaires:{})
   end
 end
--- app/models/institut.rb
+++ app/models/institut.rb
@@ -2,8 +2,13 @@ class Institut < ApplicationRecord
   belongs_to :customer
   geocoded_by :full_address
   after_validation :geocode, if: :will_save_change_to_address?
+  after_initialize :init_horaires
 
   def full_address
     [address, cp, city, country].compact.join(', ')
+  end
+
+  def init_horaires
+    self.horaires ||= { monday: {am_1:"",am_2:"",pm_1:"",pm_2:""},tuesday: {am_1:"",am_2:"",pm_1:"",pm_2:""},thursday: {am_1:"",am_2:"",pm_1:"",pm_2:""},friday: {am_1:"",am_2:"",pm_1:"",pm_2:""},saturday: {am_1:"",am_2:"",pm_1:"",pm_2:""},sunday: {am_1:"",am_2:"",pm_1:"",pm_2:""},monday: {am_1:"",am_2:"",pm_1:"",pm_2:""}}.to_json
   end
 end
