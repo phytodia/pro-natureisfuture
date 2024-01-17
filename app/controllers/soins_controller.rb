0d166b5c030a0ab9a65600c00fd990fd0eb81d77
thomas hunckler <th@phytodia.com>
2024-01-17 13:49:06 +0100

    change

--- app/controllers/soins_controller.rb
+++ app/controllers/soins_controller.rb
@@ -1,16 +1,21 @@
 class SoinsController < ApplicationController
   def index
+    @soins = Soin.all
   end
 
   def show
+    @soin = Soin.find(params[:id])
   end
 
   def visage
+    @soins = Soin.all.where(category: "visage")
   end
 
   def corps
+    @soins = Soin.all.where(category: "corps")
   end
 
   def massages
+    @soins = Soin.all.where(category: "massage")
   end
 end
