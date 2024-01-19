dd1522b2ef6ef0ba1eaa7ff6b188e82868d1cc0b
thomas hunckler <th@phytodia.com>
2024-01-19 17:37:16 +0100

    change

--- app/controllers/cartes_controller.rb
+++ app/controllers/cartes_controller.rb
@@ -16,18 +16,28 @@ class CartesController < ApplicationController
     #params[:carte][:soin_ids].each do |soin_id|
     #  @carte.soins << Soin.find(soin_id)
     #end
+    @carte.carte_soins.clear
 
     soins_nif = params[:carte][:carte_soins_attributes]
-    soins_nif.keys.each do |key|
-      x = CarteSoin.new(estimated_time: soins_nif[key]["estimated_time"],soin_id:soins_nif[key]["soin_id"])
-      x.save
-      @carte.carte_soins << x
-    end
 
+    soins_nif.keys.each do |key|
+      if soins_nif[key]["estimated_time"] != ""
+        x = CarteSoin.new(estimated_time: soins_nif[key]["estimated_time"],soin_id:soins_nif[key]["soin_id"])
+        x.save
+        @carte.carte_soins << x
+      end
+      #x = CarteSoin.new(estimated_time: soins_nif[key]["estimated_time"],soin_id:soins_nif[key]["soin_id"])
+      #x.save
+      #@carte.carte_soins << x
+    end
     params[:carte][:custom_soin_ids] = params[:carte][:custom_soin_ids].reject(&:blank?)
     params[:carte][:custom_soin_ids].each do |custom_soin_id|
-      @carte.custom_soins << CustomSoin.find(custom_soin_id)
+      x = CarteSoin.new(custom_soin_id: custom_soin_id.to_i)
+      x.save
+      @carte.carte_soins << x
+      #@carte.custom_soins << CustomSoin.find(custom_soin_id)
     end
+
 
     @carte.save!
     if @carte.save
@@ -41,9 +51,15 @@ class CartesController < ApplicationController
   end
 
   def edit
+    @carte = Carte.find(params[:id])
   end
 
   def update
+    fail
+    @institut = Institut.find(params[:institut_id])
+    @carte = Carte.find(params[:id])
+    @carte.update(carte_params)
+    redirect_to institut_espace_pro_index_path(@institut)
   end
 
   def destroy
