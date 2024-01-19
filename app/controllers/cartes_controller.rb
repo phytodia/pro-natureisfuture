28fbd3e9f554af895984808ce66d10285b2977ea
thomas hunckler <th@phytodia.com>
2024-01-19 08:46:35 +0100

    add model

---
+++ app/controllers/cartes_controller.rb
@@ -0,0 +1,20 @@
+class CartesController < ApplicationController
+  def new
+    @carte = Carte.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
