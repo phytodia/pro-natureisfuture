d8e73df990969a7e4486462fc280d179e246fe1d
thomas hunckler <th@phytodia.com>
2024-01-23 14:11:51 +0100

    change money rails

--- app/controllers/instituts_controller.rb
+++ app/controllers/instituts_controller.rb
@@ -16,9 +16,11 @@ class InstitutsController < ApplicationController
     @institut = Institut.find(params[:id])
     @flat = @institut
     @soins = []
-    @institut.carte.carte_soins.each do |soin|
-      @soins << soin.soin if soin.soin_id != nil
-      @soins << soin.custom_soin if soin.custom_soin != nil
+    if @institut.carte.present?
+      @institut.carte.carte_soins.each do |soin|
+        @soins << soin.soin if soin.soin_id != nil
+        @soins << soin.custom_soin if soin.custom_soin != nil
+      end
     end
     #require 'uri'
 
