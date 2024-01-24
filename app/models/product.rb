1bd3a91f26ac18fbdc2ab96be7717ebf23bdf8b8
thomas hunckler <th@phytodia.com>
2024-01-24 15:36:17 +0100

    add variables

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,9 @@ class Product < ApplicationRecord
   has_many :product_soin_items
   has_many :soins, through: :product_soin_items
   monetize :price_ht_cents
+
+  PREOCCUPATIONS = ["hydratation","nutrition","ride","fermeté","nettoyage","imperfection","sensibilité","tiraillement","éclat","minceur"]
+  TYPE_PRODUITS = ["crème","sérum","contour des yeux","masque","gommage","nettoyant","lotion","huile"]
   #serialize :actions, Array
   #serialize :labels, Array
   #serialize :types_peau, Array
