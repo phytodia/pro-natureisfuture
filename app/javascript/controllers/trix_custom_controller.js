6bda6497df16b4de99b701fe526e8c46912303fc
thomas hunckler <th@phytodia.com>
2024-02-15 16:48:46 +0100

    change

---
+++ app/javascript/controllers/trix_custom_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="trix-custom"
+export default class extends Controller {
+  connect() {
+    Trix.config.blockAttributes.heading2 = {
+      tagName: "h2",
+      terminal: true,
+      breakOnReturn: true,
+      group: false
+    }
+    var h2ButtonHTML = '<button type="button" data-trix-attribute="heading2" title="Subheading">H2</button>'
+    let groupElement = document.querySelector("trix-toolbar .trix-button-group")
+    //var groupElement = Trix.config.toolbar.content.querySelector(".block_tools")
+    groupElement.insertAdjacentHTML("beforeend", h2ButtonHTML)
+  }
+}
