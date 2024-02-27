2f745a327638df4a8099e69196a6b466f6433dca
thomas hunckler <th@phytodia.com>
2024-02-27 10:25:32 +0100

    change

--- app/mailers/institut_mailer.rb
+++ app/mailers/institut_mailer.rb
@@ -1,6 +1,6 @@
 class InstitutMailer < ApplicationMailer
 
   def nouvelle_demande
-    mail(to:"th@phytodia.com", subject:"Nouvelle demande de rendez-vous pour un soin",from:"no-reply@pro.natureisfuture.fr")
+    mail(to:"th@phytodia.com", subject:"Nouvelle demande de rendez-vous pour un soin")
   end
 end
