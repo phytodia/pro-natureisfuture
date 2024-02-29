b764d29ea8da5f153c9724a3b907171db0962121
thomas hunckler <th@phytodia.com>
2024-02-29 15:07:36 +0100

    add amount last years

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -25,4 +25,24 @@ class Customer < ApplicationRecord
     end
     return total_amount
   end
+
+  def total_n
+    year = Date.today.year
+    total_amount = 0
+    orders = self.orders.where("EXTRACT(year FROM custom_date) = ?", year)
+    orders.each do |order|
+      total_amount += order.amount_ht
+    end
+    return total_amount
+  end
+
+  def total_n_1
+    year = Date.today.year - 1
+    total_amount = 0
+    orders = self.orders.where("EXTRACT(year FROM custom_date) = ?", year)
+    orders.each do |order|
+      total_amount += order.amount_ht
+    end
+    return total_amount
+  end
 end
