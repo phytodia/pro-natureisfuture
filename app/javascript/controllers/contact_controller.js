b6dfe3eef503b5026a675746547308a251a443e6
thomas hunckler <th@phytodia.com>
2024-03-13 16:15:45 +0100

    change

--- app/javascript/controllers/contact_controller.js
+++ app/javascript/controllers/contact_controller.js
@@ -2,12 +2,25 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="contact"
 export default class extends Controller {
-  static targets = ["soin", "hiddenSoin"]
+  static targets = ["soin", "hiddenSoin","hiddenField"]
   connect() {
     console.log("data controller contact")
   }
   selectValue(){
     //this.soinTarget.value = this.soinTarget.options[this.soinTarget.selectedIndex].innerText
     this.hiddenSoinTarget.value = this.soinTarget.options[this.soinTarget.selectedIndex].innerText
+  }
+  objetSelect(event){
+    debugger;
+    if (event.currentTarget.value === "Devenir partenaire") {
+      this.hiddenFieldTargets.forEach((element)=>{
+        element.classList.remove("hidden-message")
+      })
+    }
+    else {
+      this.hiddenFieldTargets.forEach((element)=>{
+        element.classList.add("hidden-message")
+      })
+    }
   }
 }
