20fae4ddc33ee8837c301ad52a7f6a4513522551
thomas hunckler <th@phytodia.com>
2024-03-13 09:20:30 +0100

    change

--- app/mailers/institut_mailer.rb
+++ app/mailers/institut_mailer.rb
@@ -1,6 +1,7 @@
 class InstitutMailer < ApplicationMailer
 
   def nouvelle_demande
+    ## Check destinataire
     mail(to:"hunckler.thomas@hotmail.fr", subject:"Nouvelle demande de rendez-vous pour un soin")
   end
 end
