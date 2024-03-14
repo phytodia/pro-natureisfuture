6021b76ba3b064e2a2de7f1fffbc086eaf3c0a40
thomas hunckler <th@phytodia.com>
2024-03-14 08:25:44 +0100

    change

--- app/javascript/controllers/contact_controller.js
+++ app/javascript/controllers/contact_controller.js
@@ -11,15 +11,18 @@ export default class extends Controller {
     this.hiddenSoinTarget.value = this.soinTarget.options[this.soinTarget.selectedIndex].innerText
   }
   objetSelect(event){
-    debugger;
     if (event.currentTarget.value === "Devenir partenaire") {
       this.hiddenFieldTargets.forEach((element)=>{
         element.classList.remove("hidden-message")
+        element.querySelector("input").required = true
+        //element.attributes.required = "required"
       })
     }
     else {
       this.hiddenFieldTargets.forEach((element)=>{
         element.classList.add("hidden-message")
+        element.querySelector("input").required = false
+        //element.attributes.required = ""
       })
     }
   }
