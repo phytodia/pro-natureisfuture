82e47d13d0d2a32aebf555b77d5ddd2390d8b5cd
thomas hunckler <th@phytodia.com>
2024-03-14 14:51:50 +0100

    change trix <p> default

--- app/javascript/controllers/trix_custom_controller.js
+++ app/javascript/controllers/trix_custom_controller.js
@@ -3,6 +3,9 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="trix-custom"
 export default class extends Controller {
   connect() {
+    Trix.config.blockAttributes.default.tagName = "p"
+    Trix.config.blockAttributes.default.breakOnReturn = true
+
     Trix.config.blockAttributes.heading2 = {
       tagName: "h2",
       terminal: true,
