1561ddbf0fc2fb657e0d9c231f0649ebe9b670a3
thomas hunckler <th@phytodia.com>
2024-03-25 17:04:42 +0100

    change

--- app/controllers/crm_controller.rb
+++ app/controllers/crm_controller.rb
@@ -500,7 +500,9 @@ class CrmController < ApplicationController
       sum += order[1]
       @amount_hash[order[3].year.to_s][Date::MONTHNAMES[order[3].month].downcase][order[2]] = sum
       ## Remlissage de all dans le hash
-
+      sum_all = @amount_hash[order[3].year.to_s][Date::MONTHNAMES[order[3].month].downcase]["all"] ||= 0
+      sum_all = sum_all + order[1]
+      @amount_hash[order[3].year.to_s][Date::MONTHNAMES[order[3].month].downcase]["all"] = sum_all
     end
 
 
