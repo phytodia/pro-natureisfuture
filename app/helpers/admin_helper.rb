cf377b9d94d102d0f7dbf09a2d492709a0bad8d2
thomas hunckler <th@phytodia.com>
2024-04-05 17:26:52 +0200

    change

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -21,4 +21,33 @@ module AdminHelper
       end
     end
   end
+
+  def self.csv_to_cmd
+    require "csv"
+    filepath = "db/data/cmd_nif.csv"
+    CSV.foreach((filepath), headers: true, col_sep: ";") do |row|
+      #puts row[0] #cmd presta
+      #puts row[2] #email
+      #puts row[3] #amount_ht_cents
+      #puts row[4] #year
+      #puts row[5] #month
+      #puts row[6] #day
+      #puts "---"
+
+      custom = Customer.find_by(email:row[2].to_s)
+      customers_nil = []
+      if !custom.nil?
+        amount = row[3].to_i / 1.2
+        date = Date.new(row[4].to_i,row[5].to_i,row[6].to_i)
+        order = Order.new(customer_id:custom.id, prestashop_reference:row[0],amount_ht_cents:amount,custom_date:date)
+        if order.valid?
+          order.save!
+        else
+          order.errors
+        end
+      else
+        customers_nil << custom
+      end
+    end
+  end
 end
