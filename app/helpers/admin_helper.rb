72f9489d490afba6ec9a82fdddc1b9a71dd4326e
thomas hunckler <th@phytodia.com>
2024-04-05 15:41:57 +0200

    change

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,12 +1,17 @@
 module AdminHelper
 
   def self.csv_to_customers
+    #Pour appeler le helper depuis rails c : AdminHelper.csv_to_customers
     require "csv"
     filepath = "db/data/clients_nif.csv"
     #table = CSV.parse(File.read(filepath), headers: true)
     CSV.foreach((filepath), headers: true, col_sep: ";") do |row|
-      puts "lastname :#{row[0]}, firstname: #{row[1]}"
-      puts "--"
+      custom = Customer.new(email:row[4],password:row[5],lastname:row[0],firstname:row[1],commercial_id:row[2],code_client:row[6])
+      if custom.valid?
+        custom.save!
+      else
+        custom.errors
+      end
     end
   end
 end
