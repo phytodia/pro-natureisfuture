8ec8f118dcffc8ad27a06af5a4efed0d6dfb3e54
thomas hunckler <th@phytodia.com>
2024-04-05 15:19:12 +0200

    change csv parser

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,2 +1,12 @@
 module AdminHelper
+
+  def self.csv_to_customers
+    require "csv"
+    filepath = "db/data/clients_nif.csv"
+    #table = CSV.parse(File.read(filepath), headers: true)
+    CSV.foreach((filepath), headers: true, col_sep: ";") do |row|
+      puts "lastname :#{row[0]}, firstname: #{row[1]}"
+      puts "--"
+    end
+  end
 end
