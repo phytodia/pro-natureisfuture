7b3fc8681ae82832ac65ea50099fefe4cfb03a08
thomas hunckler <th@phytodia.com>
2024-04-08 15:58:22 +0200

    change

--- app/controllers/espace_pro_controller.rb
+++ app/controllers/espace_pro_controller.rb
@@ -93,7 +93,10 @@ class EspaceProController < ApplicationController
     @customer = current_customer
   end
   def update_profile
-    fail
+    @customer = Customer.find(params[:id])
+    @customer.update(profile_params)
+    # No need for app/views/restaurants/update.html.erb
+    redirect_to  espace_pro_path(@customer)
   end
 
   private
@@ -127,6 +130,9 @@ class EspaceProController < ApplicationController
 
   def institut_params
     params.require(:institut).permit(:name,:description,:tel,:address,:cp,:city,:country,:latitude,:longitude,:category,:fb,:ig,:tik_tok,:rdv,:mess_promo,:region,:customer_id,horaires:{},photos: [])
+  end
+  def profile_params
+    params.require(:customer).permit(:lastname,:firstname,:email,:tel,:photo)
   end
 
 end
