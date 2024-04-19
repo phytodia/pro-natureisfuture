dd57b6ba852affdab6c8443c957874d7b3239b26
thomas hunckler <th@phytodia.com>
2024-04-19 14:12:55 +0200

    change

--- app/javascript/controllers/espace_pro_controller.js
+++ app/javascript/controllers/espace_pro_controller.js
@@ -23,26 +23,4 @@ export default class extends Controller {
     this.tabContentTargets[tabIndex].classList.add("visible")
   }
 
-  send(event){
-    event.preventDefault()
-    console.log("TODO: send request in AJAX 23")
-    let formSelected = event.currentTarget
-    debugger
-    fetch(formSelected.action, {
-      method: "POST",
-      headers: { "Accept": "application/json" },
-      body: new FormData(formSelected)
-    })
-      .then(response => response.json())
-      .then((data) => {
-        if (data.inserted_item) {
-
-          // beforeend could also be dynamic with Stimulus values
-          //this.itemsTarget.querySelector("ul").remove()
-          //this.itemsTarget.insertAdjacentHTML("beforeend", data.inserted_item)
-        }
-        //this.formTarget.outerHTML = data.form
-      })
-  }
-
 }
--- app/views/espace_pro/addnote.json.builder
+++
@@ -1,1 +0,0 @@
-json.inserted_item render(partial: "espace_pro/messages_list", formats: :html, locals: {rdvs: @rdvs})
