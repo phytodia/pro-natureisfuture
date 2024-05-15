1a8dc9bbf674edaeb883f938943ca119000af381
thomas hunckler <th@phytodia.com>
2024-05-15 17:35:31 +0200

    change

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -13,8 +13,7 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/thomasphyto/clw7y6j07001q01qscu6z18ib"
-
+      style: "mapbox://styles/mapbox/streets-v10"
     })
     this.addMarkersToMap()
     this.fitMapToMarkers()
