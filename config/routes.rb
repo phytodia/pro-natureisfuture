fd96e42ae9d449f3184831871e4fea7a2a4acbbb
thomas hunckler <th@phytodia.com>
2024-06-13 10:50:24 +0200

    change

--- config/routes.rb
+++ config/routes.rb
@@ -97,7 +97,7 @@ Rails.application.routes.draw do
       get :clients, to: "admin#customers", path: "/clients"
       get :client, to: "admin#customer", path: "/clients/:id"
       get "/tous-les-cours", to: "admin#cours"
-      get "instituts", to: "admin#instituts"
+      get :instituts, to: "admin#instituts", as: :admin_instituts
     end
 
     resources :admin
