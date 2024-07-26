ce5a2e956f15707bf98e6216211996f55d0af5c7
thomas hunckler <th@phytodia.com>
2024-07-26 12:23:15 +0200

    change

--- app/controllers/slider_homes_controller.rb
+++ app/controllers/slider_homes_controller.rb
@@ -1,13 +1,16 @@
 class SliderHomesController < ApplicationController
+  def index
+    @slider = SliderHome.first
+  end
   def edit
     @slider = SliderHome.first
+    @slide = SlideItem.find(params[:id])
     #slides = SlideItem.where(slider_home_id:@slider)
-    @slides = @slider.slide_items
   end
 
   def update
-    @slider = SliderHome.first
-    @slider.update(slider_params)
+    @slide = SlideItem.find(params[:id])
+    @slide.update(slider_params)
     redirect_to edit_slider_home_path(SliderHome.first)
   end
 
