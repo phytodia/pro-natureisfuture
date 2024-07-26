bcc19cdbda2bb9d763d52c78a3837822726610ae
thomas hunckler <th@phytodia.com>
2024-07-26 14:43:10 +0200

    change

--- app/controllers/slide_items_controller.rb
+++ app/controllers/slide_items_controller.rb
@@ -1,8 +1,15 @@
 class SlideItemsController < ApplicationController
   def new
+    @slider = SliderHome.first
+    @slide = SlideItem.new
   end
 
   def create
+    @slider = SliderHome.first
+    @slide = SlideItem.new(slide_params)
+    @slide.slider_home = @slider
+    @slide.save
+    redirect_to slider_homes_path()
   end
 
   def edit
--- app/controllers/slider_homes_controller.rb
+++ app/controllers/slider_homes_controller.rb
@@ -2,17 +2,6 @@ class SliderHomesController < ApplicationController
   def index
     @slider = SliderHome.first
   end
-  def edit
-    @slider = SliderHome.first
-    @slide = SlideItem.find(params[:id])
-    #slides = SlideItem.where(slider_home_id:@slider)
-  end
-
-  def update
-    @slide = SlideItem.find(params[:id])
-    @slide.update(slider_params)
-    redirect_to edit_slider_home_path(SliderHome.first)
-  end
 
   def slider_params
     params.require(:slider_home).permit(slide_items_attributes:[:id,:link,:photo])
