65e3c5c39cfad381a36c4cd2b2df3fe55c1fd662
thomas hunckler <th@phytodia.com>
2024-07-26 14:08:38 +0200

    change

--- app/controllers/slide_items_controller.rb
+++ app/controllers/slide_items_controller.rb
@@ -6,11 +6,20 @@ class SlideItemsController < ApplicationController
   end
 
   def edit
+    @slider = SliderHome.first
+    @slide = SlideItem.find(params[:id])
   end
 
   def update
+    @slide = SlideItem.find(params[:id])
+    @slide.update(slide_params)
+    redirect_to slider_homes_path()
   end
 
   def destroy
+  end
+
+  def slide_params
+    params.require(:slide_item).permit(:id,:photo,:link)
   end
 end
