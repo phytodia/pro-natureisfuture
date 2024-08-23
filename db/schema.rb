e3ac250fb4da82a40a6cf48de845a19985b9a7b2
thomas hunckler <th@phytodia.com>
2024-08-23 13:41:47 +0200

    change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_01_094730) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_09_133546) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -97,7 +97,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_01_094730) do
 
   create_table "blog_posts", force: :cascade do |t|
     t.string "titre"
-    t.text "content"
+    t.jsonb "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "chapo"
