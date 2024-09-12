bea246190fea5710f52fe051adbc67b1e31c4d1b
thomas hunckler <th@phytodia.com>
2024-09-12 08:58:43 +0200

    change

--- app/helpers/stock_instituts_helper.rb
+++ app/helpers/stock_instituts_helper.rb
@@ -3,10 +3,18 @@ module StockInstitutsHelper
   def self.reinitialize_stock_institut
     #Pour appeler le helper depuis rails c : StockInstitutsHelper.reinitialize_stock_institut
     PdtStockItem.destroy_all
-    StockInstitut.all.each do |stock|
-      stock.init_stock
-      puts "stock crée pour #{stock.institut}"
-      sleep 2
+    StockInstitut.destroy_all
+    produits_ids = Product.where(public:true).pluck(:id).uniq
+    instituts_ids = Institut.all.pluck(:id).uniq
+    instituts_ids.each do |institut_id|
+      StockInstitut.create(institut_id:institut_id)
+      sleep 1
     end
+    StockInstitut.each do |stock|
+      produits_ids.each do |pdt_id|
+        PdtStockItem.create(stock_institut_id:stock.id,product_id:pdt_id,quantity:0)
+      end
+    end
+    puts "stock crée"
   end
 end
